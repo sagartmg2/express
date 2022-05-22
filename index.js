2b961ca10cc82882410f9c9f291ebd495fe5e97d
Sagar <dev.sagartmg@gmail.com>
2022-05-22 17:05:53 +0545

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+console.log(express);
+
+
+// git install
+ // add git cmd line to enviorntal variables
+
+// git init
+// git remote add orgin https://github.com/sagartmg2/express
+// to check git remote -v
+
+// git pull origin <branch_name>
+// git pull origin master
+
+
+// another process to pull our code
+// git clone https://github.com/sagartmg2/express
+
+
+// git status
+
+
+
