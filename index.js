a1316c6aa7f46d39c1d65735fdac00fff02d1644
Sagar <dev.sagartmg@gmail.com>
2022-05-24 10:38:01 +0545

    middleware in express.js

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
 const express = require("express")
-console.log(express);
+
+// MIDDLEWARE
+// any function that executes after receiving a request and before sending a response is a middleware
+
+// it has capability to use / alter both request and response
+
+// next => move to the next middleware in line
 
 
-// git install
- // add git cmd line to enviorntal variables
+const app = express();
 
-// git init
-// git remote add orgin https://github.com/sagartmg2/express
-// to check git remote -v
+var bodyParser = require('body-parser')
 
-// git pull origin <branch_name>
-// git pull origin master
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
+app.use(express.json());
+// app.use(<middleware>)
+// global middleware
 
-// another process to pull our code
-// git clone https://github.com/sagartmg2/express
+// app.use(ab)
 
+function ab(req, res, next) {
+    console.log("i am from middleware");
+    next();
+}
 
-// git status
+app.use((req, res, next) => {
+    // req.body = { "random": "random" }
+    // res.sendStatus(500)
+    console.log("before");
+    return next();
+    return next({ "error": "error" });
+    return;
+    console.log("after");
+})
 
+app.get("/", ab, (req, res) => {
+    console.log("hello");
+    res.send("text me");
+})
+
+app.get("/home", (req, res) => {
+    console.log("home");
+    res.send("home");
+})
+
+app.post("/signup", (req, res) => {
+    console.log("bodty", req.body);
+
+    // LET NAME = REQ.BODY.NAME
+    // LET AGE = REQ.BODY.AGE
+    // object destructuring
+    // ...rest // spread
+    let { name, age, ...rest } = req.body;
+
+    // user = user();
+    // user.age = age ;
+    
+    console.log({ name });
+    console.log({ age });
+    console.log({ rest });
+
+    console.log("signup");
+    res.send("signup");
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("404 page not found")
+})
+
+app.use((err, req, res, next) => {
+    console.log({ err });
+    res.send("Error")
+})
+
+app.listen(8000, () => {
+    console.log("listening");
+})
 
 
